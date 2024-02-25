c28a474001520e300a42ab122800159d62ff55bb
Andrew Cutler <AndrewKCutler@gmail.com>
2024-02-25 13:47:13 -0500

    created basic mapping of syllable stresses per line of text

--- index.py
+++ index.py
@@ -1,8 +1,41 @@
 import cmudict
 
-print("test")
+def get_syllable_count(phones):
+    count = 0
+    for stress in ["0", "1", "2"]:
+        for phone in phones:
+            if stress in phone:
+                count += 1
 
-print(cmudict.entries()[34443][0])
-print(cmudict.dict()['happy'])
-# print(cmudict.raw())
-# print(cmudict.words()[2342])
+    return count
+
+def map_stressed_syllables(phones):
+    # print(phones)
+    stresses = ''
+    for phone in phones:
+        # print(phone)
+        if '0' in phone:
+            stresses += '0'
+        if '1' in phone:
+            stresses += '1'
+        if '2' in phone:
+            stresses += '2'
+
+    return stresses
+
+with open('./all_star.txt', 'r') as file:
+    line_meters = []
+    for line in file:
+        line_meter = ''
+        words = [word.lower() for word in line.rstrip().split(' ')]
+        for word in words:
+            syllable_map = cmudict.dict()[word]
+            if (len(syllable_map) > 0):
+                # print(syllable_map)
+                line_meter += map_stressed_syllables(syllable_map[0])
+            else:
+                # print("Unknown word: ", word)
+                line_meter += '?' + word + '?'
+        line_meters.append(line_meter)
+
+    print(line_meters)
